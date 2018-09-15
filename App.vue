cdf504014b33febc2d78de906d85eb8a56421233
HiroshiOyamada <h5y1m141@gmail.com>
2018-09-15 12:16:47 +0900

    initial commit

---
+++ App.vue
@@ -0,0 +1,37 @@
+<template>
+  <view class="container">
+    <view class="main">
+      <button-one buttonlabel="1" />
+      <button-one buttonlabel="2" />
+      <button-one buttonlabel="3" />
+      <button-one buttonlabel="4" />
+      <button-one buttonlabel="5" />
+      <button-one buttonlabel="6" />
+      <button-one buttonlabel="7" />
+      <button-one buttonlabel="8" />
+      <button-one buttonlabel="9" />
+    </view>
+  </view>
+</template>
+
+<script>
+import ButtonOne from './src/buttonOne';
+export default {
+  components: {
+    ButtonOne
+  }
+}
+</script> 
+<style>
+.container {
+  background-color: #ddd;
+  flex: 1;
+  padding-top: 50px;
+  align-items: center;
+}
+.main {
+  flex: 1;
+  max-width: 400px;
+  align-items: center;
+}
+</style>
---
+++ src/buttonOne.vue
@@ -0,0 +1,41 @@
+<template>
+  <view class="container">
+    <touchable-opacity :on-press="check" class="button">
+      <text class="button-text">{{ buttonlabel }}</text>
+    </touchable-opacity>
+  </view>
+</template>
+
+<script>
+export default {
+  props: [
+    'buttonlabel'
+  ],
+  methods: {
+    check: function () {
+      alert("hello"+ this.buttonlabel)
+    }
+  }
+}
+</script>
+
+<style>
+.container {
+    flex-direction: row;
+    padding: 2px;  
+}
+.button {
+    flex: 1;
+    background-color: #ff3311;
+    margin-left: 5px;
+    align-items: center;
+    justify-content: center;
+    padding-top: 5px;
+    padding-bottom: 5px;
+    max-width: 80px;
+}
+.button-text {
+    color: white;
+    font-weight: normal;
+}
+</style>
